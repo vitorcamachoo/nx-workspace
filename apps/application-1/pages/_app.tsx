955f4323dff10019d4e44e4f4d9207111b1e9704
Vítor Camacho <vitor.camachoo@gmail.com>
2021-02-01 17:38:09 +0000

    temp

--- apps/application-1/pages/_app.tsx
+++ apps/application-1/pages/_app.tsx
@@ -9,6 +9,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
         <title>Welcome to application-1!</title>
       </Head>
       Branch: <i>{process.env.NEXT_PUBLIC_BRANCH}</i> <br />
+      Branch: <i>{process.env.NEXT_PUBLIC_BRANCH}</i> <br />
       Commit: 222<i>{process.env.NEXT_PUBLIC_COMMIT}</i>
     </>
   );
