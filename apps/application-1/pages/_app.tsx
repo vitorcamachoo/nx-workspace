df7ca59a98250d71efea39c595a4ded78ff1882c
Vítor Camacho <vitor.camachoo@gmail.com>
2021-02-01 10:32:31 +0000

    temp

--- apps/application-1/pages/_app.tsx
+++ apps/application-1/pages/_app.tsx
@@ -4,6 +4,7 @@ import Head from 'next/head';
 import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
 import './styles.css';
 
+console.log(process.env);
 console.log(process.env.NEXT_PUBLIC_CURRENT_BRANCH);
 
 function CustomApp({ Component, pageProps }: AppProps) {
