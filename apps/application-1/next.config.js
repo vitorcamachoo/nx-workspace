5d303b909e8cad4f4d6fd14fc2e3b94044c4f057
Vítor Camacho <vitor.camachoo@gmail.com>
2021-02-01 12:14:12 +0000

    temp

--- apps/application-1/next.config.js
+++ apps/application-1/next.config.js
@@ -1,13 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const childprocess = require('child_process')
 const withNx = require('@nrwl/next/plugins/with-nx');
-const commit = childprocess.execSync('git rev-parse HEAD').toString()
-const branch = childprocess.execSync('git rev-parse --abbrev-ref HEAD').toString()
 
 module.exports = withNx({
     target: 'experimental-serverless-trace',
     env: {
-        NEXT_PUBLIC_BRANCH: branch,
-        NEXT_PUBLIC_COMMIT: commit,
+        NEXT_PUBLIC_BRANCH: process.env.VERCEL_GIT_COMMIT_REF,
+        NEXT_PUBLIC_COMMIT: process.env.VERCEL_GIT_COMMIT_SHA,
     },
 });
