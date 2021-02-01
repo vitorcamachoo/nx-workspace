020c9a6dc83d6c5f1b03a752c7126850fe426803
Vítor Camacho <vitor.camachoo@gmail.com>
2021-02-01 17:22:33 +0000

    temp

--- apps/application-1/pages/_app.tsx
+++ apps/application-1/pages/_app.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
-import { ReactComponent as NxLogo } from '../public/nx-logo-white.svg';
-import './styles.css';
-//
-//
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,15 +8,6 @@ function CustomApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Welcome to application-1!</title>
       </Head>
-      <div className="app">
-        <header className="flex">
-          <NxLogo width="75" height="50" />
-          <h1>Welcome to application-1!</h1>
-        </header>
-        <main>
-          <Component {...pageProps} />
-        </main>
-      </div>
       Branch: <i>{process.env.NEXT_PUBLIC_BRANCH}</i> <br />
       Commit: <i>{process.env.NEXT_PUBLIC_COMMIT}</i>
     </>
