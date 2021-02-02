2fe3538b0e1652e0d2c5924cc49f557ee77c7535
Vítor Camacho <vitor.camachoo@gmail.com>
2021-02-02 09:34:30 +0000

    temp

--- apps/application-2/src/app/app.tsx
+++ apps/application-2/src/app/app.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
 
-import { ReactComponent as Logo } from './logo.svg';
-import star from './star.svg';
-
 export function App() {
   return <h1>Welcome to application-2!</h1>;
 }
