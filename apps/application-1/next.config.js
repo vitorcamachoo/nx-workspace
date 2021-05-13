91a95756c27e87bc743c3ddc19343726ffd12f24
Vitor Camacho <vitor.camachoo@gmail.com>
2021-05-13 14:59:44 +0100

    temp commit

--- apps/application-1/next.config.js
+++ apps/application-1/next.config.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
-const config = require('../../next.base.config');
 const withPWA = require('next-pwa');
 const runtimeCaching = require('next-pwa/cache');
+const config = require('../../next.config');
 
 const isProduction = process.env.NODE_ENV === 'production';
 
